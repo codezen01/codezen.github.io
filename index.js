64112e95ed2b76d65e9335a3d4f77c371f414e73
kailash.yadav <kailashyadav0411@gmail.com>
2023-04-15 22:53:15 +0530

    Added Project details for cortx

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const more = document.querySelector('.more');
 const moreDetails = document.querySelector('.more-details');
+const cortx = document.querySelector('#project1');
+const cortxDetails = document.querySelector('.cortx');
+const welcomeMessage = document.querySelector('.welcome-message');
+
 var toggle=true;
 more.addEventListener('click', function() {
     if (toggle == true) {
@@ -9,6 +13,20 @@ more.addEventListener('click', function() {
 
     else if (toggle == false) {
         moreDetails.style.display = 'none';
+        toggle = true;
+    }
+});
+
+cortx.addEventListener('click', function() {
+    if (toggle == true) {
+        cortxDetails.style.display = 'block';
+        welcomeMessage.style.display = 'none';
+        toggle = false;
+    }
+
+    else if (toggle == false) {
+        cortxDetails.style.display = 'none';
+        welcomeMessage.style.display = 'block';
         toggle = true;
     }
 });
